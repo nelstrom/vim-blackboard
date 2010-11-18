7f8cfc8868d88b16de15ed70db3a85263bd9dcb4
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 14:16:21 +0000

    Create named variables for colors.

--- src/board.rb
+++ src/board.rb
@@ -1,5 +1,13 @@
 # vim: nowrap fdm=marker
 # colors {{{1
+bright_orange = "FF5600"
+yolk_yellow   = "FFDE00"
+lemon_yellow  = "D8FA3C"
+green         = "61CE3C"
+light_blue    = "84A7C1"
+medium_grey   = "AEAEAE"
+deep_navy = "0B1022"
+white = "FFF"
 # }}}
 vim_colors "board" do
   author "Drew Neil"
@@ -7,4 +15,6 @@ vim_colors "board" do
 
   reset true 
   background :dark
+
+  Normal white, deep_navy
 end
