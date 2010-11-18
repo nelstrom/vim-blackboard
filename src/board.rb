d8e89ff9e192357089fcf6aa618e1a242198db02
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 14:20:57 +0000

    Filter all colors (some exist without names).

--- src/board.rb
+++ src/board.rb
@@ -8,6 +8,25 @@ light_blue    = "84A7C1"
 medium_grey   = "AEAEAE"
 deep_navy = "0B1022"
 white = "FFF"
+#000000
+#070a15
+#0B1022
+#191E2F
+#1C3B79
+#1d2652
+#283A76
+#323232
+#4a4a59
+#61CE3C
+#84A7C1
+#888888
+#AEAEAE
+#C23621
+#D8FA3C
+#FF5600
+#FFDE00
+#ffffaf
+#ffffff
 # }}}
 vim_colors "board" do
   author "Drew Neil"
