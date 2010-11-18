d2f5b8dc40270e13c3c66660c2affb1d2f4797ee
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 14:11:09 +0000

    Create 'board' scheme.

---
+++ src/board.rb
@@ -0,0 +1,10 @@
+# vim: nowrap fdm=marker
+# colors {{{1
+# }}}
+vim_colors "board" do
+  author "Drew Neil"
+  notes  "Based on the 'Blackboard' theme from TextMate."
+
+  reset true 
+  background :dark
+end
