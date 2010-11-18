ec89993432963c056d2eefd48507486625fc96b2
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 14:26:39 +0000

    Name all existing colors.

--- src/board.rb
+++ src/board.rb
@@ -6,27 +6,19 @@ lemon_yellow  = "D8FA3C"
 green         = "61CE3C"
 light_blue    = "84A7C1"
 medium_grey   = "AEAEAE"
-deep_navy = "0B1022"
-white = "FFF"
-#000000
-#070a15
-#0B1022
-#191E2F
-#1C3B79
-#1d2652
-#283A76
-#323232
-#4a4a59
-#61CE3C
-#84A7C1
-#888888
-#AEAEAE
-#C23621
-#D8FA3C
-#FF5600
-#FFDE00
-#ffffaf
-#ffffff
+deep_navy     = "0B1022"
+white         = "FFFFFF"
+black         = "000000"
+off_black     = "070a15"
+navy          = "191E2F"
+blue1         = "1C3B79"
+blue2         = "283A76"
+deep_blue     = "1d2652"
+dark_grey     = "323232"
+blue_grey     = "4a4a59"
+light_grey    = "888888"
+terracotta    = "C23621"
+pale_yellow   = "ffffaf"
 # }}}
 vim_colors "board" do
   author "Drew Neil"
